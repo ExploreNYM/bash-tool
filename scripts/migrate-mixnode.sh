28a9fc57a94cec52eeb28ff3c91df9d13121c07b
gabrielclcardoso <gabrielclcardoso@gmail.com>
2023-06-08 00:24:07 +0200

    Refactor: moved mixnode migration to another file

---
+++ scripts/migrate-mixnode.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Font formats
+set_bold="\033[1m"
+set_normal="\033[22m"
+
+###############
+## FUNCTIONS ##
+###############
+
+move_nym_folder() {
+	nym_path=$(sudo find / -type d -name ".nym" 2>/dev/null)
+	if [[ -n "$nym_path" ]]
+	then
+		sudo mv "$nym_path" "$HOME/"
+		echo "Folder moved successfully to $HOME"
+		sudo chown -R $USER:$USER $HOME/.nym
+	else
+		echo "Folder not found."
+		exit
+	fi
+}
+
+##############################
+## MAIN EXECUTION OF SCRIPT ##
+##############################
+
+./display-logo.sh
+echo -e "${set_bold}Mixnode Migration Started.$set_normal\n"
+move_nym_folder
+
+# now run updater
+# what's running updater?
